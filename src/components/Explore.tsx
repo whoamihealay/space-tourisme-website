bb2195fa9f799d1bf44303f0fa8c0de4f5c35e9f
Andrew Southern <whoamihealay@gmail.com>
2024-02-21 23:30:02 -0500

    fix broken link

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Explore = () => {
   return (
-    <Link href="/destination">
+    <Link href="/destinations">
       <div className="relative mx-auto my-16 flex h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-white font-serif text-xl uppercase text-dark hover:shadow-explore hover:shadow-white/10 tablet:h-60 tablet:w-60 tablet:shadow-xl desktop:h-72 desktop:w-72 desktop:shadow-2xl">
         Explore
       </div>
